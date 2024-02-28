d54c618ca6cbceb2d91b5ea67905ecb9ddc21d9c
jeorgiaobrien <j.a.obrien@wustl.edu>
2024-02-28 14:16:35 -0600

    studio 5

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -1,9 +1,13 @@
 package studio5;
 
+import java.util.Scanner;
+
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Methods {
 
+	
+	
 	/**
 	 * Compute the (Euclidean) distance between two points.
 	 *
@@ -14,7 +18,9 @@ public class Methods {
 	 * @return the Euclidean distance between (x1,y1) and (x2,y2)
 	 */
 	public static double distanceBetween(double x1, double y1, double x2, double y2) {
-		double distance = 0;
+		double distance = Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2-y1),2));
+		
+	
 		// FIXME: Hint use Math methods (e.g. Math.sqrt) to compute the distance
 		
 		return distance;
@@ -30,7 +36,13 @@ public class Methods {
 	public static void drawBullsEye(double x, double y, double radius) {
 		StdDraw.setPenColor(StdDraw.BLACK);
 		StdDraw.filledCircle(x, y, radius);
-
+		StdDraw.setPenColor(StdDraw.BLUE);
+		StdDraw.filledCircle(x, y, (3.0*radius)/4.0);
+		StdDraw.setPenColor(StdDraw.RED);
+		StdDraw.filledCircle(x, y, (1.0*radius)/2.0);
+		StdDraw.setPenColor(StdDraw.YELLOW);
+		StdDraw.filledCircle(x, y, (1.0*radius)/4.0);
+		
 		// TODO: Draw the remaining rings of the bull's eye
 		// Blue ring with 3.0/4.0 the radius
 		// suggested rgb values: 0, 109, 219
